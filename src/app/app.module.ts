a3e2f5f5859209544adeaf42534e3070f9ca8276
Prashant Gurung <prasantgrg777@gmail.com>
2023-04-18 15:01:05 +0545

    Swagger #5 issuse Solved!!!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,15 @@ import { UserModule } from 'src/user/user.module';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TOKENS } from 'src/config';
+import { JwtModule } from '@nestjs/jwt';
 
 
 @Module({
-  imports: [AuthModule, UserModule,PrismaModule],
+  imports: [AuthModule, UserModule,PrismaModule,
+  JwtModule.register({
+    secret: TOKENS.ACCESS_TOKEN_SECRET
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
